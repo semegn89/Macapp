af698e703c94212e75b085b7f12c2d3bba2260e5
semegn89 <frsgroupe@gmail.com>
2025-05-28 16:46:56 +0300

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,7 @@
-
-module.exports = router;
 const express = require('express');
 const router = express.Router();
 
-// ✅ Тестовый роут
+// ✅ Тестовый маршрут
 router.get('/test', (req, res) => {
   res.status(200).json({ message: 'Auth route is working!' });
 });
